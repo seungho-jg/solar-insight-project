c27dc97b0a16fd6409b95337f7ae8a004c4b338a
seungho-jg <tmdgh971229@gmail.com>
2025-02-04 19:38:22 +0900

    Feat: PIXI Canvas 추가 및 스프라이트 이미지(임시)

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,14 +1,43 @@
-<script>
-  import { onMount } from "svelte";
-  let map;
-  onMount(() => {
-      const container = document.getElementById('map');
+<script lang="ts">
+  import * as PIXI from 'pixi.js'
+  let map: any;
+  let container: any
+  let pixiCanvas: any
+  let sprite: PIXI.Sprite
+
+  async function init() {
+      const app = new PIXI.Application()
+      await app.init({
+        resizeTo: window,
+        backgroundAlpha: 0
+      })
+      pixiCanvas = document.getElementById('pixiCanvas');
+      pixiCanvas?.appendChild(app.canvas)
+      await PIXI.Assets.load('favicon.png');
+      sprite = PIXI.Sprite.from('favicon.png');
+      sprite.x += 10
+      sprite.y += 10
+      sprite.width = 50;  // 스프라이트 크기 조정
+      sprite.height = 50;
+      app.stage.addChild(sprite)
+    }
+
+  $effect(() => {
+    container = document.getElementById('map');
+
+    async function initMap() {
       const options = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+          center: new window.kakao.maps.LatLng(37.5665, 126.9780),
           level: 3
       };
       map = new window.kakao.maps.Map(container, options);
+      await init()
+    }
+    initMap()
+
   });
 </script>
-
-<div id="map" style="width:500px;height:400px;"></div>
+<div>
+  <div id="pixiCanvas" class="w-screen h-screen absolute z-10"></div>
+  <div id="map" class="w-screen h-screen opacity-95"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,10 @@
   <script src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${PUBLIC_KAKAO_MAP_API_KEY}`}></script> 
 </svelte:head>
 
-<section>
+<div class="w-full absolute z-20 bottom-1 flex">
+  <button class="bg-slate-400 p-3 rounded-xl mx-auto opacity-35 hover:opacity-100 transition-colors duration-75"> 견적내기</button>
+</div>
+<section class="w-full h-full">
   <Map/>
 </section>
 
