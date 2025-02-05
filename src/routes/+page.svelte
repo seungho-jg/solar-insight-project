6bb72c829f5cf2d47c95750618cbb49a0cb5bbd1
seungho-jg <tmdgh971229@gmail.com>
2025-02-05 12:38:39 +0900

    Feat: Add area selection Btn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,13 @@
   <script src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${PUBLIC_KAKAO_MAP_API_KEY}`}></script> 
 </svelte:head>
 
-<div class="w-full absolute z-20 bottom-1 flex">
-  <button class="bg-slate-400 p-3 rounded-xl mx-auto opacity-35 hover:opacity-100 transition-colors duration-75"> 견적내기</button>
+<div class="w-full z-20 bottom-4 flex flex-row fixed items-center justify-center gap-5">
+  <button class="bg-slate-400 px-6 py-3 rounded-xl opacity-35 hover:opacity-100 transition-all duration-200 shadow-lg hover:shadow-xl text-white font-semibold">
+    영역선택
+  </button>
+  <button class="bg-slate-400 px-6 py-3 rounded-xl opacity-35 hover:opacity-100 transition-all duration-200 shadow-lg hover:shadow-xl text-white font-semibold">
+    견적내기
+  </button>
 </div>
 <section class="w-full h-full">
   <Map/>
